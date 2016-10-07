2a7755e8398a7aaaef0fe4f949c8049c2669d100
hazzii <hasnain.jiwa@student.anglia.ac.uk>
2016-10-07 11:13:58 +0100

    Added 4 classes
    positions, player and draw

--- Player.cs
+++ Player.cs
@@ -3,8 +3,35 @@ using System.Windows;
 
 namespace ConsoleApplication
 {
-    
+    public class Player
+    {
+        int xPosition;
+        int yPosition;
+        char sprite;
 
+        public int XPosition
+        {
+            get { return xPosition; }
+            set { xPosition = value; }
+        }
+        public int YPosition
+        {
+            get { return yPosition; }
+            set { yPosition = value; }
+        }
 
+        public Player()
+        {
+            sprite = '&';
+            xPosition = 3;
+            yPosition = 3;
+        }
+
+        public void Draw()
+        {
+            Console.Write(sprite);
+        }
+
+    }
 
 }
