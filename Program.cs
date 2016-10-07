7795b066c8f85c3dfbbf8370df4a335c2f9bca3a
hazzii <hasnain.jiwa@student.anglia.ac.uk>
2016-10-07 11:55:55 +0100

    Added player 2...

--- Program.cs
+++ Program.cs
@@ -8,37 +8,41 @@ namespace ConsoleApplication
         {
             
             Player player1 = new Player();
-
+            Player player2 = new Player();
             
             Console.WriteLine("Hello Haz!");
             Console.ReadLine();
 
             Console.Clear();
             player1.Draw();
+            player2.Draw();
 
-            string direction = "";
-            ConsoleKeyInfo keypress = Console.ReadKey(true);
-            if(keypress.Key == ConsoleKey.UpArrow)
+            while(true)
             {
-                direction = "up";
-            }
-            if(keypress.Key == ConsoleKey.DownArrow)
-            {
-                direction = "down";
-            }
-            if(keypress.Key == ConsoleKey.LeftArrow)
-            {
-                direction = "left";
-            }
-            if(keypress.Key == ConsoleKey.RightArrow)
-            {
-                direction = "right";
-            }
+                string direction = "";
+                ConsoleKeyInfo keypress = Console.ReadKey(true);
+                if(keypress.Key == ConsoleKey.UpArrow)
+                {
+                    direction = "up";
+                }
+                if(keypress.Key == ConsoleKey.DownArrow)
+                {
+                    direction = "down";
+                }
+                if(keypress.Key == ConsoleKey.LeftArrow)
+                {
+                    direction = "left";
+                }
+                if(keypress.Key == ConsoleKey.RightArrow)
+                {
+                    direction = "right";
+                }
 
-            player1.Move(direction);
-            Console.Clear();
-            player1.Draw();
-
+                player1.Move(direction);
+                Console.Clear();
+                player1.Draw();
+                player2.Draw();
+            }
             Console.ReadKey(true);
         }
     }
