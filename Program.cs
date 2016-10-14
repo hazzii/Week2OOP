e5fa28cba0c37b306c2695b8d9e12e0679a06b09
hazzii <hasnain.jiwa@student.anglia.ac.uk>
2016-10-14 11:25:09 +0100

    Refactoring player draw
    Got rid of some redundant code, added option for player 2 to pick a
    sprite

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using System.Collections.Generic;
 
 
 namespace ConsoleApplication
@@ -8,16 +9,26 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            
-            Player player1 = new Player();
-            Player player2 = new Player();
-            
-            Console.WriteLine("Hello Haz!");
+            List<Player> players = new List<Player>();
+
+            players.Add(new Player());
+            Console.Clear();
+
+            Console.Write("Player 2, press a key to represent your player: ");
+            char playerSprite = Console.ReadKey().KeyChar;
+
+            players.Add(new Player(playerSprite));
+
+
+
+            Console.WriteLine("\n\nRetroRPG");
+            Console.WriteLine("Press any key to begin!");
             Console.ReadLine();
 
             Console.Clear();
-            player1.Draw();
-            player2.Draw();
+
+            Draw(players);
+
 
             while(true)
             {
@@ -60,13 +71,21 @@ namespace ConsoleApplication
                         direction2 = "right";
                     }
 
-                    player1.Move(direction);
-                    player2.Move(direction2);
+                    players[0].Move(direction);
+                    players[1].Move(direction2);
                 }
                     Console.Clear();
-                    player1.Draw();
-                    player2.Draw();
+                    
+                    Draw(players);
+                    
                     Thread.Sleep(1000/30);
+            }
+        }
+        public static void Draw(List<Player> players)
+        {
+            foreach(Player player in players)
+            {
+                player.Draw();
             }
         }
     }
