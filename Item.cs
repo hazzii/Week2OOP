50ab568763daa8f93e5520245e14040efdd1954b
Jiwa <HJ245@student.anglia.ac.uk>
2016-10-21 09:31:08 +0100

    Added Item.cs

---
+++ Item.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace ConsoleApplication
+{
+
+    public class Item
+    {
+        string name;
+        string description;
+        char sprite;
+        public Item()
+        {
+
+
+        }
+
+        public Item(string name, string description, char sprite)
+        {
+            this.name = name;
+            this.description = description;
+            this.sprite = sprite;
+        }
+
+
+
+    }
+
+}
